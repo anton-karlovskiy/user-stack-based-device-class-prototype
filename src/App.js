ceb8e5d2ba0c71615f568dd7b3e4549c0d52ebc3
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-07-19 19:09:02 +0300

    cloned cra-ua-aware-code-splitting approach as the beginning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { lazy, Suspense } from 'react';
+import { getDeviceClass } from "./utils/helpers";
 import './App.css';
 
-function App() {
+const Product = lazy(() => {
+  return new Promise(resolve => {
+    resolve(getDeviceClass());
+  }).then(
+    deviceClass => {
+      console.log('[ProductView] device class => ', deviceClass);
+      switch (deviceClass) {
+        case 'heavy':
+          return import(/* webpackChunkName: "heavy" */ './components/Heavy');
+        case 'light':
+          return import(/* webpackChunkName: "light" */ './components/Light');
+        default:
+          return import(/* webpackChunkName: "heavy" */ './components/Heavy');
+      }
+    }
+  );
+});
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Product imageUrl="https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" width={500} />
+        </Suspense>
       </header>
     </div>
   );
-}
+};
 
 export default App;
